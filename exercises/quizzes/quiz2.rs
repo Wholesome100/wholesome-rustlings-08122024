bfdca9c4d45936d7503965335fe9d306539f0da9
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2024-08-20 18:26:54 -0400

    Finished quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,18 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> 
+    { 
+        let mut output: Vec<String> = vec![];
+        for tuple in input {
+            match tuple.1 {
+                Command::Uppercase => output.push(tuple.0.to_uppercase()),
+                Command::Trim => output.push(tuple.0.trim().to_string()),
+                Command::Append(times) => output.push(format!("{0}{1}", tuple.0, "bar".repeat(times)))
+            }
+        }
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +48,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
